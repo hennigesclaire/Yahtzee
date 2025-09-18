88ff463ce9f37c1d17b99973abf926261030bdf3
TracksJosh <arcticwolf1526@gmail.com>
2025-09-18 10:00:59 -0500

    netbeans in github

---
+++ yahtzee_netbeans_design/src/main/java/com/mycompany/yahtzee/Yahtzee.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yahtzee;
+
+/**
+ *
+ * @author joshu
+ */
+public class Yahtzee {
+
+    public static void main(String[] args) {
+        YahtzeeDesign g = new YahtzeeDesign();
+        g.setVisible(true);
+    }
+}
