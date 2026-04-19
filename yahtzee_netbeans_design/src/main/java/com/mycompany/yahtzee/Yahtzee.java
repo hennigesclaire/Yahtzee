8c500904d6a88afbd29d347a140746fdac44a3e2
hennigesclaire <hennigesclaire@gmail.com>
2026-04-19 18:15:25 -0500

    Opens full screen?
    forgot to do in last one

--- yahtzee_netbeans_design/src/main/java/com/mycompany/yahtzee/Yahtzee.java
+++ yahtzee_netbeans_design/src/main/java/com/mycompany/yahtzee/Yahtzee.java
@@ -9,13 +9,14 @@ package com.mycompany.yahtzee;
  * @author joshu
  */
 
-import java.awt.Font;
-import java.util.Random;
+import javax.swing.JFrame;
+
 public class Yahtzee {
 
 public static void main(String[] args) {
     javax.swing.SwingUtilities.invokeLater(() -> {
         StartPageDesign startPage = new StartPageDesign();
+        startPage.setExtendedState(JFrame.MAXIMIZED_BOTH);
         startPage.setVisible(true);
     });
     }
